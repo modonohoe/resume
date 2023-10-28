4b658bfabaa7e1150a37d1619918de0c8d6b4d48
modonohoe <>
2023-10-28 00:20:18 +0000

    Revert to tutorial coe

--- assets/maps.js
+++ assets/maps.js
@@ -1,49 +1,25 @@
 function initMap() {
-    // Request needed libraries.
-    const { Map, InfoWindow } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
-        "marker",
-    );
-    const map = new google.maps.Map(document.getElementById("map"), {
+    var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
-        center: { lat: -28.024, lng: 140.887 },
-        mapId: "DEMO_MAP_ID",
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
     });
 
-    const infoWindow = new google.maps.InfoWindow({
-        content: "",
-        disableAutoPan: true,
-    });
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    // Create an array of alphabetical characters used to label the markers.
-    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    const locations = [
+    var locations = [
         { lat: 53.297902, lng: -6.179645 },
         { lat: 53.332203, lng: -6.255906 },
     ];
 
-    // Add some markers to the map.
-    const markers = locations.map(function (location, i) {
-        const label = labels[i % labels.length];
-        const pinGlyph = new google.maps.marker.PinElement({
-            glyph: label,
-            glyphColor: "white",
-        });
-        const marker = new google.maps.marker.AdvancedMarkerElement({
+    var markers = locations.map(function (location, i) {
+        return new google.maps.Marker({
             position: location,
-            content: pinGlyph.element,
+            label: labels[i % labels.length]
         });
-
-        // markers can only be keyboard focusable when they have click listeners
-        // open info window when marker is clicked
-        marker.addListener("click", () => {
-            infoWindow.setContent(position.lat + ", " + position.lng);
-            infoWindow.open(map, marker);
-        });
-        return marker;
     });
 
-    // Add a marker clusterer to manage the markers.
-    new MarkerClusterer({ markers, map });
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
